c92a0327edca41580c1b5ba517baca82985cc40b
Moha <mmjafari@openmailbox.org>
2019-03-23 16:04:23 +0900

    changed filename switch

--- Argon.sh
+++ Argon.sh
@@ -2,7 +2,7 @@
 # Argon, 3rd try
 arHelp="Usage:\n-o\t--output\t\t\t declare output file name\n-a\t--action {inject,remove,add}\t set the action\n-h\t--help\t\t\t\t show this page and exit\n"
 
-findPlace () { #TODO complete this part, path to item.
+findPlace () {
 	lineNumber="$(cat $fileName | grep $1 --line-number | sed 's/ *:.*//')"
 	echo $(cat $fileName | head -n $lineNumber | sed 's/ //g' | grep --basic-regexp '^(\|^)' | sed 's/(//g') | sed 's/ /\n/g' > tmp
 	sed -i "$(($(cat tmp | grep ')' --line-number | sed 's/ *:.*//') - 1))d" tmp
@@ -13,10 +13,11 @@ findPlace () { #TODO complete this part, path to item.
 	exit
 }
 
-
+addSub () { #TODO complete this part. adding a sub to a class/subclass
+}
 while [ '$1' != '' ] ; do
 	case $1 in
-		-o | --output )		shift
+		-f | --file-name )		shift
 					fileName=$1
 					;;
 		-a | --action )		shift
