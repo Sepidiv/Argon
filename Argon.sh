ef8a49b9b71fb901dbdb6e962039560b1919d6d8
Moha <mmjafari@openmailbox.org>
2019-03-23 15:57:20 +0900

    Created Argon.sh

---
+++ Argon.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Argon, 3rd try
+arHelp="Usage:\n-o\t--output\t\t\t declare output file name\n-a\t--action {inject,remove,add}\t set the action\n-h\t--help\t\t\t\t show this page and exit\n"
+
+findPlace () { #TODO complete this part, path to item.
+	lineNumber="$(cat $fileName | grep $1 --line-number | sed 's/ *:.*//')"
+	echo $(cat $fileName | head -n $lineNumber | sed 's/ //g' | grep --basic-regexp '^(\|^)' | sed 's/(//g') | sed 's/ /\n/g' > tmp
+	sed -i "$(($(cat tmp | grep ')' --line-number | sed 's/ *:.*//') - 1))d" tmp
+	sed -i 's/)//g' tmp
+	sed -i '/^$/d' tmp
+	echo "$(echo $(cat tmp) | sed 's/ /=>/g')=>$1"
+	rm tmp
+	exit
+}
+
+
+while [ '$1' != '' ] ; do
+	case $1 in
+		-o | --output )		shift
+					fileName=$1
+					;;
+		-a | --action )		shift
+					action=$1
+					;;
+		-p | --place )		shift
+					xDim=$1
+					shift
+					yDim=$1
+					;;
+		-w | --find-place )	shift
+					findPlace $1
+					;;
+		-h | --help )		echo -ne $arHelp
+					exit
+					;;
+	esac
+	shift
+done
